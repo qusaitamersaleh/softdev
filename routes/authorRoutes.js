aaf41569afd4ce8ab390c74251159acf1f5e063d
wabtech <48417913+qusaitamersaleh@users.noreply.github.com>
2022-01-10 20:55:04 +0200

    add the routes

--- routes/authorRoutes.js
+++ routes/authorRoutes.js
@@ -7,6 +7,8 @@ const router = express.Router();
 // Protect all routes after this middleware
  
 router.post('/add-author', AuthorConroller.addAuthor);
- 
+router.get('/get-authors', AuthorConroller.getAuthors);
+router.get('/get-author/:id', AuthorConroller.getAuthorDetails);
+
 
 module.exports = router;
