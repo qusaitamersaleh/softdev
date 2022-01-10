bc9ee4d6424689cd59b439ee87652a171aa6897d
wabtech <48417913+qusaitamersaleh@users.noreply.github.com>
2022-01-10 23:37:54 +0200

    patch to put

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post('/add-book', BookConroller.addBook);
 router.get('/get-books', BookConroller.getBooks);
 router.get('/get-book/:id', BookConroller.getBookDetails);
-router.patch('/edit-book/:id', BookConroller.editBook);
+router.put('/edit-book/:id', BookConroller.editBook);
 
 
 
