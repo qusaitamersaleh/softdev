d584f4023c22d0cecccf56da65d5e8ff2db9431b
wabtech <48417913+qusaitamersaleh@users.noreply.github.com>
2022-01-10 15:56:49 +0200

    setting up + strcutre

---
+++ routes/authoerRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const AuthorConroller = require('../controller/authorController');
+ 
+const router = express.Router();
+
+
+// Protect all routes after this middleware
+ 
+router.post('/send-message', chatC.sendMessage);
+ 
+const router = express.Router();
+
+ 
+
+module.exports = router;
